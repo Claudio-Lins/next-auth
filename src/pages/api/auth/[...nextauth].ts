4ad9c74bc3a0ca38e178490dcdbed9f2ee02ed01
Claudio Lins <claudio.lins@me.com>
2022-06-05 13:25:14 +0100

    email provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,38 +1,65 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
 import GithubProvider from "next-auth/providers/github";
-import prisma from "../../../../lib/prisma";
+import GoogleProvider from "next-auth/providers/google";
+import { db } from '../../../../lib/db.server'
+
+const data = db.user.findMany();
+
+const prisma = new PrismaClient()
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-      profile(profile) {
-        return {
-          id: profile.id.toString(),
-          name: profile.name || profile.login,
-          username: profile.login,
-          email: profile.email,
-          image: profile.avatar_url,
-        };
-      },
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
     }),
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    //   profile(profile) {
+    //     return {
+    //       id: profile.id.toString(),
+    //       name: profile.name || profile.login,
+    //       username: profile.login,
+    //       email: profile.email,
+    //       image: profile.avatar_url,
+    //     };
+    //   },
+    // }),
     // ...add more providers here
   ],
   secret: process.env.NEXT_AUTH_SECRET,
   adapter: PrismaAdapter(prisma),
   callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-        username: user.username,
-      },
-    }),
-  },
+    async signIn({
+      account, profile
+    }) {
+      if(account.provider === 'google') {
+        return profile.email_verified && profile.email.endsWith('@gmail.com')
+    }
+      return true
+  }
+  }
+  // callbacks: {
+  //   session: ({ session, user }) => ({
+  //     ...session,
+  //     user: {
+  //       ...session.user,
+  //       id: user.id,
+  //       username: user.username,
+  //     },
+  //   }),
+  // },
 };
 
 export default NextAuth(authOptions);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { getSession, signOut, useSession } from "next-auth/react";
+import { getSession, signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -7,6 +7,7 @@ import useRequireAuth from "../../lib/useRequireAuth";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
+  console.log('SessionProvider', session);
 
   // const router = useRouter()
 
@@ -25,29 +26,17 @@ const Home: NextPage = () => {
         <h1 className="text-6xl font-bold">
           Welcome{" "}
           <a className="text-blue-600" href="https://nextjs.org">
-            {session ? session.user.name : "Guest"}
+            {session ? session.user.email : "Guest"}
           </a>
         </h1>
         <button
-          onClick={() => signOut()}
+          onClick={() => session ? signOut() : signIn()}
           type="button"
           className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block mt-10"
         >
-          <span className="inline-block mr-2">Sair</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-4 h-4 inline-block"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M17 8l4 4m0 0l-4 4m4-4H3"
-            />
-          </svg>
+          <span className="inline-block mr-2">
+            {session ? 'SignOut' : 'SignIn'}
+          </span>
         </button>
       </main>
     </div>
@@ -56,21 +45,21 @@ const Home: NextPage = () => {
 
 export default Home;
 
-// export async function getServerSideProps(context) {
-//   const session = await getSession(context);
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
 
-//   if (!session) {
-//     return {
-//       redirect: {
-//         destination: "/login",
-//         permanent: false,
-//       },
-//     };
-//   }
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
 
-//   return {
-//     props: {
-//       session,
-//     },
-//   };
-// }
+  return {
+    props: {
+      session,
+    },
+  };
+}
